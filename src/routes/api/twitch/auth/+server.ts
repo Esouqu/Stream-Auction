bca68e28ba7c42dd591b89866fd515bca2b8ae6b
esouqu <nippl3juic3@gmail.com>
2023-10-24 02:54:15 +0700

    remove one scope option from twitch auth

--- src/routes/api/twitch/auth/+server.ts
+++ src/routes/api/twitch/auth/+server.ts
@@ -2,7 +2,7 @@ import { PUBLIC_TWITCH_CLEINT_ID } from "$env/static/public";
 import { redirect, type RequestHandler } from "@sveltejs/kit";
 
 export const GET: RequestHandler = async () => {
-  const twitchScope = 'channel:manage:redemptions+channel:read:redemptions+moderator:manage:announcements';
+  const twitchScope = 'channel:manage:redemptions+channel:read:redemptions';
   const twitchRedirectUrl = 'http://localhost:5173/redirect/twitch';
   const queryParams = `response_type=code&client_id=${PUBLIC_TWITCH_CLEINT_ID}&redirect_uri=${twitchRedirectUrl}&scope=${twitchScope}`;
   const twitchAuthUrl = `https://id.twitch.tv/oauth2/authorize?${queryParams}`;
