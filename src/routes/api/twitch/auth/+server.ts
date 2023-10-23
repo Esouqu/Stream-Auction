8dc94c06c7540444ff191d46f988fae9a4e0ced7
esouqu <nippl3juic3@gmail.com>
2023-10-24 02:55:57 +0700

    change redirect url for twitch integration

--- src/routes/api/twitch/auth/+server.ts
+++ src/routes/api/twitch/auth/+server.ts
@@ -3,7 +3,7 @@ import { redirect, type RequestHandler } from "@sveltejs/kit";
 
 export const GET: RequestHandler = async () => {
   const twitchScope = 'channel:manage:redemptions+channel:read:redemptions';
-  const twitchRedirectUrl = 'http://localhost:5173/redirect/twitch';
+  const twitchRedirectUrl = 'https://stream-auction.vercel.app/redirect/twitch';
   const queryParams = `response_type=code&client_id=${PUBLIC_TWITCH_CLEINT_ID}&redirect_uri=${twitchRedirectUrl}&scope=${twitchScope}`;
   const twitchAuthUrl = `https://id.twitch.tv/oauth2/authorize?${queryParams}`;
 
