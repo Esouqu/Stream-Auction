c3984c1301ee6a14bcf84e4c02ab2623aab0b0f3
esouqu <nippl3juic3@gmail.com>
2024-04-14 18:25:54 +0700

    added vercel analytics

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,4 +1,5 @@
 import { dev } from '$app/environment';
+import { inject } from '@vercel/analytics'
 import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
 
 if (!dev) injectSpeedInsights();
