1dc4e28785c9027f84ac98c8a691351f3cd3a8d8
esouqu <nippl3juic3@gmail.com>
2024-06-30 14:37:30 +0700

    logging

--- src/routes/api/seen-updates/+server.ts
+++ src/routes/api/seen-updates/+server.ts
@@ -1,7 +1,7 @@
 import type { RequestHandler } from "@sveltejs/kit";
 
 export const POST: RequestHandler = async ({ cookies }) => {
-  const date = JSON.stringify(new Date());
+  const date = new Date().toISOString();
 
   cookies.set('lastSeenUpdates', date, { path: '/' });
 
