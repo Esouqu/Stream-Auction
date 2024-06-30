6ae85db9eadad274550805ae759ebe753120debb
esouqu <nippl3juic3@gmail.com>
2024-06-30 16:15:49 +0700

    removing unnecessary +page.server files

--- src/routes/wheel/+page.server.ts
+++
@@ -1,8 +0,0 @@
-import type { PageServerLoad } from "./$types";
-
-export const load: PageServerLoad = async ({ locals }) => {
-  return {
-    haveSeenWarning: locals.haveSeenWarning,
-    haveSeenUpdates: locals.haveSeenUpdates
-  }
-};
