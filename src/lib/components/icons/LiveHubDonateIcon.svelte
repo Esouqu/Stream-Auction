1867644c3c88fbd1aae0003e2ede51dd13c17458
romkij <romkiij@gmail.com>
2025-03-17 22:57:18 +0300

    Added LiveHub small logo in donation feed.

---
+++ src/lib/components/icons/LiveHubDonateIcon.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import logo from '$lib/assets/livehub_small.png';
+    interface Props {
+        class?: string;
+        size?: number;
+    }
+
+    const {class: className, size = 32}: Props = $props();
+</script>
+<img src="{logo}" alt="LiveHub" class={className} width={size} height={size} draggable="false" />
